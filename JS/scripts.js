6790faad0eaf2fb4ace410dc0176e8c08c089f89
JD_Rodrigues <jdevproject01@gmail.com>
2022-03-29 18:53:01 -0300

    top-scroll

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,4 @@
+
 let postsList = document.querySelector('.latest-posts-list-footer')
 let tweetsList = document.querySelector('.latest-tweets-list-footer')
 
@@ -41,3 +42,27 @@ function slideUp(el) {
         }
     },5)
 }
+
+window.onscroll=function (){
+    let card1 = document.querySelector('.card1')
+    let card2 = document.querySelector('.card2')
+    let card3 = document.querySelector('.card3')
+    let fcard1 = document.querySelector('.footer-card1')
+    let fcard2 = document.querySelector('.footer-card2')
+    let fcard3 = document.querySelector('.footer-card3')
+
+    if (card1.getBoundingClientRect().top<=580) {
+        card1.style.opacity='1.0'
+        card2.style.left="0"
+        card3.style.left="0"
+    }    
+
+    if (fcard1.getBoundingClientRect().top<=580) {
+        fcard1.style.opacity='1.0'
+        fcard2.style.left="0"
+        fcard3.style.left="0"
+    }
+}
+
+
+
