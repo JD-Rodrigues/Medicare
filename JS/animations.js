c4ad1d7fb0bec6581de0b3723cb654cfa14a0c47
JD_Rodrigues <jdevproject01@gmail.com>
2022-03-29 20:35:15 -0300

    Animação dos divisores de categorias

---
+++ JS/animations.js
@@ -0,0 +1,29 @@
+window.onscroll=function (){
+    let card1 = document.querySelector('.card1')
+    let card2 = document.querySelector('.card2')
+    let card3 = document.querySelector('.card3')
+    let fcard1 = document.querySelector('.footer-card1')
+    let fcard2 = document.querySelector('.footer-card2')
+    let fcard3 = document.querySelector('.footer-card3')
+    let categoryDivisor = document.querySelectorAll('.divisor-category')
+
+    if (card1.getBoundingClientRect().top<=580) {
+        card1.style.opacity='1.0'
+        card2.style.left="0"
+        card3.style.left="0"
+    }    
+
+    if (fcard1.getBoundingClientRect().top<=580) {
+        fcard1.style.opacity='1.0'
+        fcard2.style.left="0"
+        fcard3.style.left="0"
+    }
+
+    for (i in categoryDivisor) {
+        if (categoryDivisor[i].getBoundingClientRect().top<=580) {
+            categoryDivisor[i].style.width='50px'
+        }
+    }
+    
+}
+
--- JS/scripts.js
+++ JS/scripts.js
@@ -43,26 +43,5 @@ function slideUp(el) {
     },5)
 }
 
-window.onscroll=function (){
-    let card1 = document.querySelector('.card1')
-    let card2 = document.querySelector('.card2')
-    let card3 = document.querySelector('.card3')
-    let fcard1 = document.querySelector('.footer-card1')
-    let fcard2 = document.querySelector('.footer-card2')
-    let fcard3 = document.querySelector('.footer-card3')
-
-    if (card1.getBoundingClientRect().top<=580) {
-        card1.style.opacity='1.0'
-        card2.style.left="0"
-        card3.style.left="0"
-    }    
-
-    if (fcard1.getBoundingClientRect().top<=580) {
-        fcard1.style.opacity='1.0'
-        fcard2.style.left="0"
-        fcard3.style.left="0"
-    }
-}
-
 
 
