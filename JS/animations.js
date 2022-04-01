3f9fa29e660ad984e3d1c69135800c0d176a1ec0
JD_Rodrigues <jdevproject01@gmail.com>
2022-04-01 18:23:25 -0300

    Animação dos cards adaptadapara o mobile.

--- JS/animations.js
+++ JS/animations.js
@@ -1,5 +1,9 @@
 
-onscroll=function (){
+window.addEventListener('load', slideToRight)
+
+window.addEventListener('scroll', slideToRight)
+
+function slideToRight(){
     let card1 = document.querySelector('.card1')
     let card2 = document.querySelector('.card2')
     let card3 = document.querySelector('.card3')
@@ -8,7 +12,7 @@ onscroll=function (){
     let fcard3 = document.querySelector('.footer-card3')
     let categoryDivisor = document.querySelectorAll('.divisor-category')
 
-    if (card1.getBoundingClientRect().top<=580) {
+    if (card1.getBoundingClientRect().top<=570) {
         card1.style.opacity='1.0'
         card2.style.left="0"
         card3.style.left="0"
